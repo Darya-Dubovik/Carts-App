11cc6580a789440a91343aeeeb3771ee68efd9cc
Darya <dashka_ivashenko@mail.ru>
2025-12-23 13:12:11 +0300

    fix(babel): add classic runtime to fix an error

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,13 +1,15 @@
-// const plugins = [];
-// module.exports = {
-//   presets: ["@babel/preset-env", "@babel/preset-react"],
-//   plugins,
-// };
-
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "classic", // <- это ключевой момент
+      },
+    ],
+  ],
   plugins: [isDevelopment && require.resolve("react-refresh/babel")].filter(
     Boolean
   ),

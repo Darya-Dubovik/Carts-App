bf305981dfd0594f5e1618cd8434102a1391f139
Darya <dashka_ivashenko@mail.ru>
2026-01-07 18:23:58 +0300

    chore(readme, build): update README and rebuild dist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./style.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
 
@@ -10,9 +10,9 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename="/Carts-App/">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
